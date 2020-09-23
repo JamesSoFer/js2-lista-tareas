4825bf54b3c396758590592b7ff6adee8bb2451c
JamesSoFer <66284023+JamesSoFer@users.noreply.github.com>
2020-09-22 22:03:23 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,3 +2,34 @@
 // Lista de tareas
 //
 
+const formulario = document.getElementById('new-task-form');
+let contadorTareas = 0;
+const lista = document.getElementById('task-list');
+const tareas = [];
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const item = document.createElement('li');
+    item.className = 'task-list_item';
+
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('id', `tarea-${contadorTareas}`);
+
+    const label = document.createElement('label');
+    label.setAttribute('for', `tarea-${contadorTareas}`);
+    label.innerHTML = formulario.elements[0].value;
+
+    item.appendChild(checkbox);
+    item.appendChild(label);
+    lista.appendChild(item);
+    const miTarea = {
+        id: contadorTareas;
+        nombre: formulario.elements[0].value,
+        completo: false,
+    };
+    tareas.push(miTarea);
+    console.log(tareas);
+    contadorTareas++;
+    formulario.elements[0].value = '';
+})
